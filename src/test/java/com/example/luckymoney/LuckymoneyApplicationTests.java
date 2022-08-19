2bea3e580b62b753c7065cd8d39e1cad259f28b3
guolin <guolin1633@163.com>
2022-08-19 15:52:31 +0800

    5th 分离开发和上线环境 两套yml

--- src/test/java/com/example/luckymoney/LuckymoneyApplicationTests.java
+++ src/test/java/com/example/luckymoney/LuckymoneyApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LuckymoneyApplicationTests {
+public class LuckymoneyApplicationTests {
 
     @Test
     void contextLoads() {
